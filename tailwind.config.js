16db351b10e6dab628b970cd926f74bbbbd8a769
Alexander Latyshev <aladdelinxr@gmail.com>
2023-07-18 04:29:38 +0300

    Configured tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+
+const { colors: defaultColors } = require("tailwindcss/defaultTheme");
+
+const colors = {
+  ...defaultColors,
+  ...{
+    transparent: "transparent",
+    primary: "rgb(249,168,212)",
+    secondary: "rgb(222, 213, 213)",
+  },
+};
+
+const width = {
+  700: "700px",
+};
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
+    screens: {
+      mobile: "240px",
+      laptop: "768px",
+      dekstop: "1024px",
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      big: "1.3rem",
+    },
+    extend: {
+      colors: colors,
+      width: width,
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
